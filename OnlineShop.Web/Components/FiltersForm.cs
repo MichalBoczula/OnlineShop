18c6dd22279f17177df284ee534b4a820b3454f4
MichalBoczula <michal.boczula0101@gmail.com>
2021-02-22 01:24:45 +0100

    Added FiltersForm as ViewComponent

---
+++ OnlineShop.Web/Components/FiltersForm.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Web.Models.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Web.Components
+{
+    public class FiltersForm : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var VM = new Filters();
+            return View(VM);
+        }
+    }
+}
